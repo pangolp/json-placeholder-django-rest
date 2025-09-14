fb24ffc08fce65de6912515301c6d7b4b96f37e0
Walter Pagani <paganiwalter@gmail.com>
2025-09-14 18:38:30 -0300

    chore (source) Update and rewrite the code (#17)

---
+++ api/urls.py
@@ -0,0 +1,24 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from .views import (
+    GeoViewSet, AddressViewSet, CompanyViewSet,
+    UserViewSet, TodoViewSet, PostViewSet,
+    CommentViewSet, AlbumViewSet, PhotoViewSet
+)
+
+
+router = routers.DefaultRouter()
+router.register('geos', GeoViewSet)
+router.register('addresses', AddressViewSet)
+router.register('companies', CompanyViewSet)
+router.register('users', UserViewSet)
+router.register('todos', TodoViewSet)
+router.register('posts', PostViewSet)
+router.register('comments', CommentViewSet)
+router.register('albums', AlbumViewSet)
+router.register('photoes', PhotoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
